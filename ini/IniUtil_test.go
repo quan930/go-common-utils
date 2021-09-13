2fa9dab423572306d3459ecf48133a483f42d1e9
LiangquanLi930 <liangli@redhat>
2021-09-13 15:45:39 +0800

    ini util update

--- ini/IniUtil_test.go
+++ ini/IniUtil_test.go
@@ -6,6 +6,6 @@ func TestIniGetKey(t *testing.T) {
 	Init("./test.ini")
 	name := IniGetKey("default", "Name")
 	if name != "test" {
-		t.Errorf("Fib(%v,%v) = %v; expected %v", "default", "Name", name, "test")
+		t.Errorf("IniGetKey(%v,%v) = %v; expected %v", "default", "Name", name, "test")
 	}
 }
